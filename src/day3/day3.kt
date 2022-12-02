92c6c8cf1ce92866c55b498c49d1e1e905f95d59
Matthias Jakob <Matthias.Jakob@bmwgroup.com>
2022-12-02 12:25:03 +0100

    Prepare day 3

--- src/day3/day3.kt
+++ src/day3/day3.kt
@@ -2,14 +2,17 @@ package day3
 
 import inputTextOfDay
 import testTextOfDay
+
 fun part1(text: String): Int {
     val lines = text.lines()
     return 0
 }
+
 fun part2(text: String): Int {
     val lines = text.lines()
     return 0
 }
+
 fun main() {
     val day = 3
 
