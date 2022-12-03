ff188e6d79af83ac7e788b9045442070f0fe5322
Matthias Jakob <Matthias.Jakob@bmwgroup.com>
2022-12-03 09:06:53 +0100

    Solve day3

--- src/day3/day3.kt
+++ src/day3/day3.kt
@@ -3,21 +3,34 @@ package day3
 import inputTextOfDay
 import testTextOfDay
 
-fun part1(text: String): Int {
-    val lines = text.lines()
-    return 0
-}
+private val Char.priority: Int
+    get() = when (this) {
+        in 'a'..'z' -> this - 'a' + 1
+        else -> this - 'A' + 27
+    }
 
-fun part2(text: String): Int {
-    val lines = text.lines()
-    return 0
-}
+fun part1(text: String): Int =
+    text.lines()
+        .map { it.chunked(it.length / 2) }
+        .map { (a, b) -> a.first { char -> b.count { it == char } >= 1 }
+        }
+        .sumOf { it.priority }
+
+fun part2(text: String): Int =
+    text.lines()
+        .chunked(3)
+        .map { (a, b, c) ->
+            a.first { item ->
+                b.count { it == item } >= 1 && c.count() { it == item } >= 1
+            }
+        }
+        .sumOf { it.priority }
 
 fun main() {
     val day = 3
 
     val testInput = testTextOfDay(day)
-    check(part1(testInput) == 0)
+    check(part1(testInput) == 157)
 
     val input = inputTextOfDay(day)
 
