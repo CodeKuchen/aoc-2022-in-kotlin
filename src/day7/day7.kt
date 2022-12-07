726c23b913bb96ea0a0f989fe728b7f781d9d96d
Matthias Jakob <Matthias.Jakob@bmwgroup.com>
2022-12-07 11:05:20 +0100

    Refactor day 7

--- src/day7/day7.kt
+++ src/day7/day7.kt
@@ -3,30 +3,31 @@ package day7
 import inputTextOfDay
 import testTextOfDay
 
-fun part1(input: String): Long {
+fun part1(input: String): Int {
+    val maxSize = 100_000
     return scanDirectorySizes(input)
         .toList()
-        .filter { (_, size) -> size <= 100_000 }
-        .sumOf { (_, size) -> size }
+        .filter { it <=  maxSize }
+        .sumOf { it }
 }
 
-fun part2(input: String): Long {
+fun part2(input: String): Int {
     val dirSizes = scanDirectorySizes(input)
 
-    val total = 70_000_000L
-    val required = 30_000_000L
-    val used = dirSizes["/"]!!
+    val total = 70_000_000
+    val required = 30_000_000
+    val used = dirSizes.first()
     val freeSpace = total - used
     val additional = required - freeSpace
 
     return dirSizes
-        .filter { (_, size) -> size > additional }
-        .minOf { (_, size) -> size }
+        .filter { it > additional }
+        .minOf { it }
 }
 
-fun scanDirectorySizes(input: String): MutableMap<String, Long> {
+fun scanDirectorySizes(input: String): List<Int> {
     val path = mutableListOf<String>()
-    val dirSizes = mutableMapOf("/" to 0L)
+    val dirSizes = mutableMapOf("/" to 0)
 
     input.lines().forEach {
         val command = it.split(" ")
@@ -37,14 +38,14 @@ fun scanDirectorySizes(input: String): MutableMap<String, Long> {
                     else -> path.add(command[2])
                 }
             }
-            "dir" -> dirSizes[path.joinToString("") + command[1]] = 0L
+            "dir" -> {}
             else -> path.forEachIndexed { index, _ ->
                 val currentPath = path.take(index + 1).joinToString("")
-                dirSizes[currentPath] = dirSizes[currentPath]!!.toLong() + command[0].toLong()
+                dirSizes[currentPath] = dirSizes.getOrDefault(currentPath,0) + command[0].toInt()
             }
         }
     }
-    return dirSizes
+    return dirSizes.map { (_, size) -> size }
 }
 
 fun main() {
@@ -54,14 +55,14 @@ fun main() {
     println("Test1: " + part1(testInput))
     println("Test2: " + part2(testInput))
 
-    check(part1(testInput) == 95437L)
-    check(part2(testInput) == 24933642L)
+    check(part1(testInput) == 95437)
+    check(part2(testInput) == 24933642)
 
     val input = inputTextOfDay(day)
 
     println("Part1: " + part1(input))
     println("Part2: " + part2(input))
 
-    check(part1(input) == 1845346L)
-    check(part2(input) == 3636703L)
+    check(part1(input) == 1845346)
+    check(part2(input) == 3636703)
 }
