3e0e215d530605d5abae3ce1a8652050dd61bac1
Matthias Jakob <Matthias.Jakob@bmwgroup.com>
2022-12-07 09:59:15 +0100

    Solve day 7

--- src/day7/day7.kt
+++ src/day7/day7.kt
@@ -3,34 +3,46 @@ package day7
 import inputTextOfDay
 import testTextOfDay
 
-typealias File = Pair<Long, String>
+var path = mutableListOf<String>()
+var dirSizes = mutableMapOf("/" to 0L)
 
-fun part1(input: String): Long {
-    var path = mutableListOf<String>()
-    var dirSizes = mutableMapOf("/" to 0L)
+fun scanDirectory(input: String) {
+    path = mutableListOf()
+    dirSizes = mutableMapOf("/" to 0L)
 
     input.lines().forEach {
         val command = it.split(" ")
-        when (command[0]){
+        when (command[0]) {
             "$" -> when(command[1]) {
                 "cd" -> when (command[2]) {
                     ".." -> path.removeLast()
                     else -> path.add(command[2])
                 }
-                "ls" -> if (dirSizes.containsKey(path.last()) ) dirSizes[path.last()] = 0L
             }
-            "dir" -> dirSizes[command[1]] = 0L
-            else -> path.forEach{ segment ->
-                dirSizes[segment] = dirSizes[segment]!!.toLong() + command[0].toLong()
+            "dir" -> dirSizes[path.joinToString("")+command[1]] = 0L
+            else -> path.forEachIndexed { index, segment ->
+                val currentPath = path.take(index+1).joinToString("")
+                dirSizes[currentPath] = dirSizes[currentPath]!!.toLong() + command[0].toLong()
             }
         }
     }
+}
+fun part1(input: String): Long {
+    scanDirectory(input)
 
-    return dirSizes.toList().filter{(dir, size) -> size <= 100_000 }.sumOf { (dir, size) -> size }
+    return dirSizes.toList().filter{(_, size) -> size <= 100_000 }.sumOf { (_, size) -> size }
 }
 
-fun part2(input: String): Int {
-    return 0
+fun part2(input: String): Long {
+    scanDirectory(input)
+
+    val total = 70_000_000L
+    val required = 30_000_000L
+    val used = dirSizes["/"]!!
+    val freeSpace = total - used
+    val additional = required - freeSpace
+
+    return dirSizes.filter { (_, size) -> size > additional }.minOf { (_, size) -> size }
 }
 
 fun main() {
@@ -41,13 +53,13 @@ fun main() {
     println("Test2: " + part2(testInput))
 
     check(part1(testInput) == 95437L)
-    check(part2(testInput) == 0)
+    check(part2(testInput) == 24933642L)
 
     val input = inputTextOfDay(day)
 
     println("Part1: " + part1(input))
     println("Part2: " + part2(input))
 
-    check(part1(input) != 1540861L)
-    //check(part2(input) == 0)
+    check(part1(input) == 1845346L)
+    check(part2(input) == 3636703L)
 }
